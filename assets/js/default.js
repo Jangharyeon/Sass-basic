1f1344a157c2216fa5f2f4208bdd9b89ff2ac242
장하련 <jangharyun@janghalyeon-ui-iMac.local>
2014-09-29 19:52:17 +0900

    ing

--- assets/js/default.js
+++ assets/js/default.js
@@ -7,3 +7,7 @@ if (is_Mac) {
 if (is_Win) {
 	$('html').addClass('win');
 }
+// IE7 .text-field + button style 추가
+if (navigator.userAgent.match(/MSIE [7]\./)) {
+	$('.text-field + button.btn').css('margin-left', '4px');
+}
